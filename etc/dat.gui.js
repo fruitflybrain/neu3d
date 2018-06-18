062048f591f66fd0a2eb11706f1369c2389faa97
Tingkai <tl2747@columbia.edu>
2018-06-18 15:47:09 -0400

    added custom fontawesome icon by layering

--- etc/dat.gui.js
+++ etc/dat.gui.js
@@ -23,24 +23,30 @@ function setCommandIcon(faName,displayType='strip',attrs={}) {
     // __li is the root dom element of each controller
     if (faName) {
         this.__li.setAttribute('icon', faName);
-
-        let faicon = document.createElement('i');
-        faicon.setAttribute("class", faName);
-        if (!(Object.keys(attrs).length === 0 && attrs.constructor === Object)){
-            for (let key in attrs) {
-                faicon.setAttribute(key, attrs[key]);
-            }    
+        let faicon;
+        let faiconHTML;
+        if (!(faName in customFA)){
+            faicon = document.createElement('i');
+            faicon.setAttribute("class", faName);
+            if (!(Object.keys(attrs).length === 0 && attrs.constructor === Object)) {
+                for (let key in attrs) {
+                    faicon.setAttribute(key, attrs[key]);
+                }
+            }
+            faiconHTML = faicon.outerHTML;
+        }else{
+            faiconHTML = customFA[faName];
         }
 
         if (displayType == 'strip'){
             this.__li.innerHTML = '';
             this.__li.style['display'] = 'inline';
             this.__li.style['border'] = 'none';
-            this.__li.prepend(faicon);
+            this.__li.innerHTML = faiconHTML;
         } else if (displayType == 'dropdown'){
-            this.__li.childNodes[0].prepend(faicon);
+            this.__li.childNodes[0].innerHTML = faiconHTML + this.__li.childNodes[0].innerHTML;
         } else{
-            this.__li.childNodes[0].prepend(faicon);
+            this.__li.childNodes[0].innerHTML = faiconHTML + this.__li.childNodes[0].innerHTML;
             console.warn("display Type = " + displayType+ " not recognized, default to `dropdown`");
         }
         
@@ -71,5 +77,9 @@ eachController(controller => {
         controller.prototype.icon = setCommandIcon;
     }
 });
+
+var customFA = {
+    'fa fa-map-upin': '<span class="fa-layers fa-fw" ><i class= "fas fa-map-pin"></i><i class="fa-inverse fas fa-times" data-fa-transform="shrink-5 right-3 down-5" style="color:red"></i></span>'
+}
 
 export default dat;
