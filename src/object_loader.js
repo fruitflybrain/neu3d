881fbbd5da7f571a769ebb2da112ba7b980a383d
Tingkai Liu <tl2747@columbia.edu>
2022-04-07 20:56:36 -0400

    refactoring object loader based on latest threejs

--- src/object_loader.js
+++ src/object_loader.js
@@ -2,15 +2,19 @@ import { Neu3D } from './neu3d';
 import {
   Vector3, Face3, Object3D, Matrix4, Mesh,
   MeshLambertMaterial, MeshBasicMaterial, PointsMaterial, LineBasicMaterial,
-  Geometry, CylinderGeometry, SphereGeometry, TubeGeometry,
+  BufferGeometry, CylinderGeometry, SphereGeometry, TubeGeometry,
   QuadraticBezierCurve3, VertexColors, LineSegments,
   XHRLoader
 } from 'three';
 import {
   SceneUtils
 } from 'three/examples/jsm/utils/SceneUtils';
+import {
+  Geometry
+} from 'three/examples/jsm/deprecated/Geometry';
 
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { Float32BufferAttribute } from 'three';
 
 /** Clip value in between min/max */
 Math.clip = function (number, min, max) {
@@ -20,8 +24,13 @@ Math.clip = function (number, min, max) {
   return Math.max(min, Math.min(number, max));
 }
 
+/** Check if SWC object is valid or not
+ *
+ * @param {Map} swcObj
+ * @returns Boolean indicating if SWC is valid
+ */
 function verifySWC(swcObj) {
-  const isValid = (val)=> ('type' in val) && ('x' in val) && ('y' in val) && ('z' in val) && ('r' in val) && ('parent' in val);
+  const isValid = (val) => ('type' in val) && ('x' in val) && ('y' in val) && ('z' in val) && ('radius' in val) && ('parent' in val);
 
   for (const [sample, value] of Object.entries(swcObj)){
     if (!isValid(value)) {
@@ -32,13 +41,66 @@ function verifySWC(swcObj) {
       return false
     }
   }
+  return true;
+}
+
+/**
+ * Convert string of SWC to json object
+ * @param {String} swcString
+ * @param {Map} scale
+ * @param {Map} rotation
+ * @param {Map} shift
+ * @returns object of swcString key-ed by id of each node
+ */
+function parseSWC(swcString, scale, rotation, shift) {
+  var swcObj = {};
+  swcString = swcString.replace(/\r\n/g, "\n");
+  swcString.split("\n").forEach((e) => {
+    let seg = e.split(' ');
+    // if is comma delimted, split again
+    if (seg.length == 1) {
+      seg = e.split(',');
+    }
+    // if has leading comma, take the rest of the slice
+    if (seg.length == 8) {
+      seg = seg.slice(1);
+    }
+
+    // only process if have the right length
+    if (seg.length == 7) {
+      var x_put = parseFloat(seg[2]) * scale.x;
+      var y_put = parseFloat(seg[3]) * scale.y;
+      var z_put = parseFloat(seg[4]) * scale.z;
+      var x_i = Math.cos(rotation.xy) * x_put + Math.sin(rotation.xy) * y_put;
+      var y_i = Math.sin(rotation.xy) * x_put + Math.cos(rotation.xy) * y_put;
+      var z_i = z_put;
+
+      var x_f = x_i;
+      var y_f = Math.cos(rotation.yz) * y_i + Math.sin(rotation.yz) * z_i;
+      var z_f = Math.sin(rotation.yz) * y_i + Math.cos(rotation.yz) * z_i;
+
+      x_f = x_f + shift.x;
+      y_f = y_f + shift.y;
+      z_f = z_f + shift.z;
+
+      swcObj[parseInt(seg[0])] = {
+        'type': parseInt(seg[1]),
+        'x': x_f,
+        'y': y_f,
+        'z': z_f,
+        'radius': parseFloat(seg[5]) * scale.radius,
+        'parent': parseInt(seg[6]),
+      };
+    }
+  });
+  return swcObj;
 }
 
 /**
 * Register Object to `meshDict`
-* @param {*} key 
-* @param {*} unit 
-* @param {*} object 
+* @param {*} key
+* @param {*} unit
+* @param {*} object
 */
 Neu3D.prototype._registerObject = function (key, unit, object) {
   object.rid = key; // needed rid for raycaster reference
@@ -72,10 +134,12 @@ Neu3D.prototype._registerObject = function (key, unit, object) {
   this.meshDict[key] = unit;
 }
 
-/** TODO: Add comment
- * @param {*} key 
- * @param {*} unit 
- * @param {*} visibility 
+/** Load Mesh from JSON into MeshDict
+ * Used primarily for loading background objects
+ *
+ * @param {String} key
+ * @param {*} unit
+ * @param {Boolean} visibility
  */
 Neu3D.prototype.loadMeshCallBack = function (key, unit, visibility) {
   return (json_or_string) => {
@@ -88,10 +152,13 @@ Neu3D.prototype.loadMeshCallBack = function (key, unit, visibility) {
       json = unit;
     }
     let color = unit['color'];
-    let geometry = new Geometry();
+
+    // Create new Buffer Geometry from mesh vertices and faces
+    let geometry = new BufferGeometry();
     let vtx = json['vertices'];
     let idx = json['faces'];
 
+    // check if vertics and indices match dimensionality
     if (Math.max(...idx) >= (vtx.length/3)){
       let err = `[Neu3D] Mesh ${key} has face index ${Math.max(...idx)} that exceeds maximum number of vertices ${vtx.length/3}.`;
       if (Math.min(...idx) !== 0) {
@@ -101,23 +168,26 @@ Neu3D.prototype.loadMeshCallBack = function (key, unit, visibility) {
       return;
     }
 
-    for (let j = 0; j < vtx.length / 3; j++) {
-      let x = parseFloat(vtx[3 * j + 0]);
-      let y = parseFloat(vtx[3 * j + 1]);
-      let z = parseFloat(vtx[3 * j + 2]);
-      geometry.vertices.push(new Vector3(x, y, z));
-      this.updateObjectBoundingBox(unit, x, y, z);
-      this.updateBoundingBox(x, y, z);
-    }
-    for (let j = 0; j < idx.length / 3; j++) {
-      geometry.faces.push(new Face3(parseInt(idx[3 * j + 0]), parseInt(idx[3 * j + 1]), parseInt(idx[3 * j + 2])));
-    }
-    geometry.mergeVertices();
-    geometry.computeFaceNormals();
+    geometry.setIndex(idx);
+    geometry.setAttribute('position', new Float32BufferAttribute(vtx, 3));
+
+    // for (let j = 0; j < vtx.length / 3; j++) {
+    //   let x = parseFloat(vtx[3 * j + 0]);
+    //   let y = parseFloat(vtx[3 * j + 1]);
+    //   let z = parseFloat(vtx[3 * j + 2]);
+    //   geometry.vertices.push(new Vector3(x, y, z));
+    //   this.updateObjectBoundingBox(unit, x, y, z);
+    //   this.updateBoundingBox(x, y, z);
+    // }
+    // for (let j = 0; j < idx.length / 3; j++) {
+    //   geometry.faces.push(new Face3(parseInt(idx[3 * j + 0]), parseInt(idx[3 * j + 1]), parseInt(idx[3 * j + 2])));
+    // }
+    // geometry.mergeVertices();
+    // geometry.computeFaceNormals();
     geometry.computeVertexNormals();
     let materials = [
       //new THREE.MeshPhongMaterial( { color: color, flatShading: true, shininess: 0, transparent: true } ),
-      new MeshLambertMaterial({ color: color, transparent: true, side: 2, flatShading: true }),
+      new MeshLambertMaterial({ color: color, transparent: true, side: 2}),
       new MeshBasicMaterial({ color: color, wireframe: true, transparent: true })
     ];
 
@@ -131,109 +201,31 @@ Neu3D.prototype.loadMeshCallBack = function (key, unit, visibility) {
 }
 
 
-// /** Load Mesh from JSON into MeshDict
-//  * Used primarily for loading background meshes
-//  * 
-//  * @param {string} key name of the mesh loaded
-//  * @param {*} unit 
-//  * @param {boolean} visibility if the mesh should be visible
-//  */
-// Neu3D.prototype.loadMeshCallBack = function(key, unit, visibility) {
-//   return (jsonString) => {
-//     let json = JSON.parse(jsonString);
-//     let color = unit['color'];
-//     let geometry = new BufferGeometry();
-//     let vtx = json['vertices'];
-//     let idx = json['faces'];
-//     // for (let j = 0; j < vtx.length / 3; j++) {
-//     //   let x = parseFloat(vtx[3 * j + 0]);
-//     //   let y = parseFloat(vtx[3 * j + 1]);
-//     //   let z = parseFloat(vtx[3 * j + 2]);
-//     //   geometry.vertices.push(new Vector3(x, y, z));
-//     //   this.updateObjectBoundingBox(unit, x, y, z);
-//     //   this.updateBoundingBox(x, y, z);
-//     // }
-//     // for (let j = 0; j < idx.length / 3; j++) {
-//     //   geometry.faces.push(new Face3(parseInt(idx[3 * j + 0]), parseInt(idx[3 * j + 1]), parseInt(idx[3 * j + 2])));
-//     // }
-//     // geometry.mergeVertices();
-//     geometry.setAttribute('position',  new Float32BufferAttribute( vtx, 3 ))
-//     geometry.setIndex(idx);
-//     geometry.computeFaceNormals();
-//     geometry.computeVertexNormals();
-
-//     geometry.computeBoundingBox();
-//     const { x, y, z } = geometry.boundingBox.max;
-//     this.updateObjectBoundingBox(unit, x, y, z);
-//     this.updateBoundingBox(x, y, z);
-//     let materials = [
-//       new MeshLambertMaterial({ color: color, transparent: true, side: 2, flatShading: true }),
-//       new MeshBasicMaterial({ color: color, wireframe: true, transparent: true })
-//     ];
-
-//     let object = SceneUtils.createMultiMaterialObject(geometry, materials);
-//     if (!this.settings.meshWireframe){
-//       object.children[1].visible = false;
-//     }
-//     object.visible = visibility;
-//     this._registerObject(key, unit, object);
-//   };
-// }
-
-
-
-/** TODO: Add comment
-* @param {*} key 
-* @param {*} unit 
-* @param {*} visibility 
+/** Load Neuron from SWC file
+* @param {String} key Name
+* @param {*} unit
+* @param {Boolean} visibility where it's visible
 */
 Neu3D.prototype.loadSWCCallBack = function (key, unit, visibility) {
   return (swc_or_string) => {
     let swcObj = {};
     if (typeof swc_or_string === 'string' || swc_or_string instanceof String){
       /** process string */
-      let swcString = swc_or_string.replace(/\r\n/g, "\n");
-      let swcLine = swcString.split("\n");
-      let radius_scale = unit['radius_scale'];
-      swcLine.forEach((e) => {
-        let seg = e.split(' ');
-        if (seg.length == 1) {
-          seg = e.split(',');
-        }
-        if (seg.length == 8) {
-          seg = seg.slice(1);
-        }
-        if (seg.length == 7) {
-          var x_put = parseFloat(seg[2]) * unit['x_scale'];
-          var y_put = parseFloat(seg[3]) * unit['y_scale'];
-          var z_put = parseFloat(seg[4]) * unit['z_scale'];
-          var x_i = Math.cos(unit['xy_rot']) * x_put + Math.sin(unit['xy_rot']) * y_put;
-          var y_i = Math.sin(unit['xy_rot']) * x_put + Math.cos(unit['xy_rot']) * y_put;
-          var z_i = z_put;
-          var y_f = Math.cos(unit['yz_rot']) * y_i + Math.sin(unit['yz_rot']) * z_i;
-          var z_f = Math.sin(unit['yz_rot']) * y_i + Math.cos(unit['yz_rot']) * z_i;
-          var x_f = x_i;
-          x_f = x_f + unit['x_shift'];
-          y_f = y_f + unit['y_shift'];
-          z_f = z_f + unit['z_shift'];
-
-          swcObj[parseInt(seg[0])] = {
-            'type': parseInt(seg[1]),
-            'x': x_f,
-            'y': y_f,
-            'z': z_f,
-            'radius': parseFloat(seg[5]) * radius_scale,
-            'parent': parseInt(seg[6]),
-          };
-        }
-      });
+      swcObj = parseSWC(
+        swc_or_string,
+        {x: unit['x_scale'], y:unit['y_scale'], z:unit['z_scale'], radius:unit['radius_scale']},
+        {xy: unit['xy_rot'], yz:unit['yz_rot']},
+        {x: unit['x_shift'], y:unit['y_shift'], z:unit['z_shift']}
+      );
     } else{
-      if (!verifySWC(swc_or_string)){
-        return;
-      }
       swcObj = swc_or_string;
     }
-    
+
+    // verify the parsed SWC object
+    if (!verifySWC(swcObj)){
+      return;
+    }
+
     let len = Object.keys(swcObj).length;
     let color = unit['color'];
     let object = new Object3D();
@@ -363,11 +355,11 @@ Neu3D.prototype.loadSWCCallBack = function (key, unit, visibility) {
   };
 }
 
-/** TODO: Add comment
-* 
-* @param {*} key 
-* @param {*} unit 
-* @param {*} visibility 
+/** Load Morphology JSON
+*
+* @param {*} key
+* @param {*} unit
+* @param {*} visibility
 */
 Neu3D.prototype.loadMorphJSONCallBack = function (key, unit, visibility) {
   return () => {
@@ -541,6 +533,13 @@ Neu3D.prototype.loadMorphJSONCallBack = function (key, unit, visibility) {
   };
 }
 
+/** Load Obj file
+ *
+ * @param {*} key
+ * @param {*} unit
+ * @param {*} visibility
+ * @returns
+ */
 Neu3D.prototype.loadObjCallBack = function (key, unit, visibility) {
   return () => {
     // instantiate a loader
